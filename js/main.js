48a5529563dc2298bb3a0bae1323d6b06b24603e
behrmart <bfelipemm@gmail.com>
2023-04-18 18:03:19 -0600

    2304181803

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 // Dove Clone Wars in JavaScript
 // By Bernardo Felipe Martinez Meave
-// @Don_linux
+// @behrmart
 // Just a little programming exercise
 // Stardate: 20230418
 
+const locations = ["Manhattan", "The Bronx", "Brooklyn", "Conney Island", "Queens"];
+const drugs = ["Cocaine", "Heroin", "Acid", "Weed", "Speed", "Ludes"];
+
+var wallet = 100;
+var trenchcoat = 100;
+var gun = false;
