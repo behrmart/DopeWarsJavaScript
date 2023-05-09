2f508015643d7e7b32b89793771511451b8ec66b
Bernardo <bfelipemm@gmail.com>
2023-05-09 16:48:20 -0600

    Fixed location button added display location change in JS

--- js/main.js
+++ js/main.js
@@ -28,7 +28,30 @@ function todayStatus(a){
 
 function travelTo (b) {
 
+    let locationcont = document.getElementById('locationcont'); //locationid container
+    let locationClassList = locationcont.classList;
+    console.log(b)
+    console.log(locationClassList);
+    let classAttrib = locationcont.getAttribute('class');
+    console.log(classAttrib)
+    locationcont.setAttribute('class', 'col-md-6 border rounded-4'); // Reset locationcont class when the travelTo is called. 
+    
+    if (b==='Manhattan'){       
+        locationcont.classList.add('text-primary-emphasis','bg-primary-subtle','border-primary-subtle'); 
+    } else if (b==='The Bronx'){
+        locationcont.classList.add('text-danger-emphasis','bg-danger-subtle','border-danger-subtle');
+    } else if (b==='Queens'){
+        locationcont.classList.add('text-success-emphasis','bg-success-subtle','border-success-subtle');
+    } else if (b==='Brooklyn'){
+        locationcont.classList.add('text-warning-emphasis','bg-warning-subtle','border-warning-subtle');
+    } else if (b==='Conney Island'){
+        locationcont.classList.add('text-info-emphasis','bg-info-subtle','border-info-subtle');
+    }
+
+
+    dayNumber ++;
     document.getElementById("locationid").innerHTML = b;
+    document.getElementById("dayid").innerHTML = "Day " + dayNumber + "/30";
 }
 
 function drugCard(a){
@@ -45,20 +68,17 @@ function nextDay(){
 }
 
 
-function main() {
+/* function main() {
     
     todayStatus(dayNumber);
+    travelTo("Manhattan");
     drugCard("Weed");
-
     
 }
 
-main();
+main(); */
 
-function change() {
-    var buttonTravel = document.getElementById('travelButton');
-    console.log("buttonTravel value:" + buttonTravel.value);
-}
+
 
 
 // Echar los Perros en JavaScript
